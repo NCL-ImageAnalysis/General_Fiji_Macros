0e7adb73c093585ff30e7f28296d17f03aef4e7e
James Grimshaw <jameswgrimshaw@gmail.com>
2025-03-11 12:32:09 +0000

    comments

--- OlgaPreProcess.py
+++ OlgaPreProcess.py
@@ -44,38 +44,53 @@ imp = IJ.getImage()
 # Z Project the image
 projected = ZProjector.run(imp, projectiontype)
 
+# Segmentation of cell + Nuclei----------------------------------------------------v
 # Selects the dapi channel
 projected.setC(dapichannel)
+# Get just the dapi channel
 dapi = projected.crop("whole-slice")
 IJ.run(dapi, "Gaussian Blur...", "sigma=40")
+# Gets the nuclei-----------------------------------------------------------------v
 JustNuclei = dapi.duplicate()
 IJ.setAutoThreshold(JustNuclei, "Yen dark")
 IJ.run(JustNuclei, "Convert to Mask", "")
 nucleirois = analyzeParticles(JustNuclei, str(nuclei_size) + "-Infinity", "0-1.0")
+#---------------------------------------------------------------------------------^
+# Delete the pixels within the nuclei
 for roi in nucleirois:
 	projected.setRoi(roi)
 	IJ.run(projected, "Clear", "stack")
 JustNuclei.close()
+
+# Gets the cell-------------------------------------------------------------------v
 dapi.setAutoThreshold("Otsu dark")
 IJ.run(dapi, "Convert to Mask", "")
 IJ.run(dapi, "Fill Holes", "")
 IJ.run(dapi, "Create Selection", "")
 CellRoi = dapi.getRoi()
+#---------------------------------------------------------------------------------^
 dapi.close()
-
+# Delete the pixels outside the cell
 projected.setRoi(CellRoi)
 IJ.run(projected, "Clear Outside", "stack")
+#----------------------------------------------------------------------------------^
+
+# Gets a binary mask of the cell
 BinaryPlus = ImagePlus(imp.getTitle() + "_mask", projected.createRoiMask())
 BinaryPlus.setCalibration(projected.getCalibration())
+# Remove the nuclei from the binary mask
 for roi in nucleirois:
 	BinaryPlus.setRoi(roi)
 	IJ.run(BinaryPlus, "Clear", "stack")
 BinaryPlus.deleteRoi()
 projected.deleteRoi()
+# Auto contrast on slices
 for chan in range(1, projected.getNChannels() + 1):
 	projected.setC(chan)
 	IJ.run(projected, "Enhance Contrast", "saturated=0.35")
 projected.setC(1)
+
+# Display projected image and binary mask
 projected.show()
 BinaryPlus.show()
 
