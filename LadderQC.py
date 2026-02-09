fd94f9ecaa62ce327c73865275c099512d5814dd
James Grimshaw <jameswgrimshaw@gmail.com>
2026-02-09 13:09:15 +0000

    moved to main function

--- LadderQC.py
+++ LadderQC.py
@@ -173,189 +173,194 @@ def selectWindow(Pattern):
 			return True
 	return False
 
+def main(
+		input_image,
+		output_directory):
+	# This section sets the measurements that will be used
+	AnalyzerClass = Analyzer()
+	# Gets original measurements to reset later
+	OriginalMeasurements = AnalyzerClass.getMeasurements()
 
-# This section sets the measurements that will be used
-AnalyzerClass = Analyzer()
-# Gets original measurements to reset later
-OriginalMeasurements = AnalyzerClass.getMeasurements()
+	# Sets the measurements to be used
+	AnalyzerClass.setMeasurements(
+		Measurements.SHAPE_DESCRIPTORS 
+		+ Measurements.CENTROID
+	)
 
-# Sets the measurements to be used
-AnalyzerClass.setMeasurements(
-	Measurements.SHAPE_DESCRIPTORS 
-	+ Measurements.CENTROID
-)
+	# Gets the needed paths and filenames for input and output
+	FileName = input_image.getName()
+	FileNameNoExtension = ".".join(FileName.split("."))[:-1]
+	OutputPath = output_directory.getPath()
 
-# Gets the needed paths and filenames for input and output
-FileName = InputImage.getName()
-FileNameNoExtension = ".".join(FileName.split("."))[:-1]
-OutputPath = OutputDirectory.getPath()
+	# Imports the image using Bioformats-------------------v
+	Options = ImporterOptions()
+	Options.setId(input_image.getPath())
+	# Ensures that the image is not split into focal planes
+	Options.setSplitFocalPlanes(False)
+	Options.setAutoscale(True)
+	Imp = BF.openImagePlus(Options)[0]
+	#------------------------------------------------------^
 
-# Imports the image using Bioformats-------------------v
-Options = ImporterOptions()
-Options.setId(InputImage.getPath())
-# Ensures that the image is not split into focal planes
-Options.setSplitFocalPlanes(False)
-Options.setAutoscale(True)
-Imp = BF.openImagePlus(Options)[0]
-#------------------------------------------------------^
+	Calibration = Imp.getCalibration()
+	ZDepth = Calibration.pixelDepth
 
-Calibration = Imp.getCalibration()
-ZDepth = Calibration.pixelDepth
+	# Max intensity of the image to get all of the ladder
+	Projected = ZProjector.run(Imp, "max")
+	# Removes the scale so ROI coordinates are correct
+	Projected.removeScale()
+	# Thresholds the image to get the ladder
+	IJ.setAutoThreshold(Projected, "Default dark")
+	IJ.run(Projected, "Convert to Mask", "")
+	# Runs analyze particles to get a list of ROIs
+	RoiList = analyzeParticles(Projected, "10-Infinity", "0.00-1.00")
 
-# Max intensity of the image to get all of the ladder
-Projected = ZProjector.run(Imp, "max")
-# Removes the scale so ROI coordinates are correct
-Projected.removeScale()
-# Thresholds the image to get the ladder
-IJ.setAutoThreshold(Projected, "Default dark")
-IJ.run(Projected, "Convert to Mask", "")
-# Runs analyze particles to get a list of ROIs
-RoiList = analyzeParticles(Projected, "10-Infinity", "0.00-1.00")
+	# String needed to get the centroid of the ROI
+	CentroidString = ["X", "Y"]
 
-# String needed to get the centroid of the ROI
-CentroidString = ["X", "Y"]
+	# Gets the centroid of each ROI and adds to a list-------------------v
+	PointList = []
+	for ThisRoi in RoiList:
+		Centroid = getRoiMeasurements(ThisRoi, Projected, CentroidString)
+		# Must be a tuple to be hashable in dictionary
+		PointList.append(tuple(Centroid))
+	#--------------------------------------------------------------------^
 
-# Gets the centroid of each ROI and adds to a list-------------------v
-PointList = []
-for ThisRoi in RoiList:
-	Centroid = getRoiMeasurements(ThisRoi, Projected, CentroidString)
-	# Must be a tuple to be hashable in dictionary
-	PointList.append(tuple(Centroid))
-#--------------------------------------------------------------------^
-
-# For each point it will get the angle of the line between it and the closest point-v
-# This will be used to eliminate the points that are not part of the ladder
-# As these points will all be running parallel to each other
-PointDict = {}
-RoundedAngleList = []
-for Index, PointItem in enumerate(PointList):
-	# Need to deep copy the list to avoid modifying the original
-	InputList = copy.copy(PointList)
-	# Need to remove the current point from the list to avoid finding itself
-	del InputList[Index]
-	# Finds the closest point to the current point
-	ClosestPoint = closestPoint(PointItem, InputList)
-	# Gets the angle between the two points
-	LineAngle = getAngleBetweenPoints(PointItem, ClosestPoint)
-	# Rounds the angle to the nearest 5 degrees
-	# Has to be absolute as the lines can be in either direction
-	RoundedAngle = abs(roundToBase(LineAngle, 5))
-	# Adds the angle to a dictionary with the point as the key
-	PointDict[PointItem] = RoundedAngle
-	# Adds the angle to a list of all angles to find the mode
-	RoundedAngleList.append(RoundedAngle)
-#-----------------------------------------------------------------------------------^
-
-# Gets the mode angle
-ModeAngle = Counter(RoundedAngleList).most_common(1)[0][0]
-# Gets the points that have the mode angle--------v
-# These are the points that are part of the ladder
-LadderList = []
-for PointItem in PointDict:
-	if ModeAngle == PointDict[PointItem]:
-		LadderList.append(PointItem)
-#-------------------------------------------------^
-
-# Gets the two points that are furthest apart but are still parallel to each other--------------------------v
-MaxLadderDistance = 0
-for Index, FirstPoint in enumerate(LadderList):
-	# Need to deep copy the list to avoid modifying the original
-	SecondList = copy.copy(LadderList)
-	# Need to remove the current point from the list to avoid finding itself
-	del SecondList[Index]
-	# Loops though every other list of points to find the furthest apart
-	for SecondPoint in SecondList:
-		# Gets the angle between the two points, has to be absolute as the lines can be in either direction
-		LadderAngle = abs(getAngleBetweenPoints(FirstPoint, SecondPoint))
-		# Gets the distance between the two points
-		LadderDistance = distanceBetweenPoints(FirstPoint[0], FirstPoint[1], SecondPoint[0], SecondPoint[1])
+	# For each point it will get the angle of the line between it and the closest point-v
+	# This will be used to eliminate the points that are not part of the ladder
+	# As these points will all be running parallel to each other
+	PointDict = {}
+	RoundedAngleList = []
+	for Index, PointItem in enumerate(PointList):
+		# Need to deep copy the list to avoid modifying the original
+		InputList = copy.copy(PointList)
+		# Need to remove the current point from the list to avoid finding itself
+		del InputList[Index]
+		# Finds the closest point to the current point
+		ClosestPoint = closestPoint(PointItem, InputList)
+		# Gets the angle between the two points
+		LineAngle = getAngleBetweenPoints(PointItem, ClosestPoint)
 		# Rounds the angle to the nearest 5 degrees
-		RoundedLadderAngle = roundToBase(LadderAngle, 5)
-		# If the angle is the same as the mode angle and the distance is greater than the current max
-		# Then these are the new furthest apart points
-		if RoundedLadderAngle == ModeAngle and LadderDistance > MaxLadderDistance:
-			FeducialLine = Line(FirstPoint[0], FirstPoint[1], SecondPoint[0], SecondPoint[1])
-			# This angle is not rounded as it is used to rotate the image
-			FeducialAngle = LadderAngle
-			MaxLadderDistance = LadderDistance
-#-----------------------------------------------------------------------------------------------------------^
+		# Has to be absolute as the lines can be in either direction
+		RoundedAngle = abs(roundToBase(LineAngle, 5))
+		# Adds the angle to a dictionary with the point as the key
+		PointDict[PointItem] = RoundedAngle
+		# Adds the angle to a list of all angles to find the mode
+		RoundedAngleList.append(RoundedAngle)
+	#-----------------------------------------------------------------------------------^
 
-# Need to use an overlay so it will rotate with the image
-LineOverlay = Overlay(FeducialLine)
-Imp.setOverlay(LineOverlay)
+	# Gets the mode angle
+	ModeAngle = Counter(RoundedAngleList).most_common(1)[0][0]
+	# Gets the points that have the mode angle--------v
+	# These are the points that are part of the ladder
+	LadderList = []
+	for PointItem in PointDict:
+		if ModeAngle == PointDict[PointItem]:
+			LadderList.append(PointItem)
+	#-------------------------------------------------^
 
-# Rotates the image so the ladder is horizontal
-IJ.run(Imp, "Arbitrarily...", "angle=" + str(FeducialAngle) + " interpolate stack")
-# Gets the Rotated Roi from the overlay
-RotatedLineOverlay = Imp.getOverlay()
-RotatedLineRoi = RotatedLineOverlay.get(0)
-# Removes the overlay to clean up the image
-Imp.setOverlay(None)
+	# Gets the two points that are furthest apart but are still parallel to each other--------------------------v
+	MaxLadderDistance = 0
+	for Index, FirstPoint in enumerate(LadderList):
+		# Need to deep copy the list to avoid modifying the original
+		SecondList = copy.copy(LadderList)
+		# Need to remove the current point from the list to avoid finding itself
+		del SecondList[Index]
+		# Loops though every other list of points to find the furthest apart
+		for SecondPoint in SecondList:
+			# Gets the angle between the two points, has to be absolute as the lines can be in either direction
+			LadderAngle = abs(getAngleBetweenPoints(FirstPoint, SecondPoint))
+			# Gets the distance between the two points
+			LadderDistance = distanceBetweenPoints(FirstPoint[0], FirstPoint[1], SecondPoint[0], SecondPoint[1])
+			# Rounds the angle to the nearest 5 degrees
+			RoundedLadderAngle = roundToBase(LadderAngle, 5)
+			# If the angle is the same as the mode angle and the distance is greater than the current max
+			# Then these are the new furthest apart points
+			if RoundedLadderAngle == ModeAngle and LadderDistance > MaxLadderDistance:
+				FeducialLine = Line(FirstPoint[0], FirstPoint[1], SecondPoint[0], SecondPoint[1])
+				# This angle is not rounded as it is used to rotate the image
+				FeducialAngle = LadderAngle
+				MaxLadderDistance = LadderDistance
+	#-----------------------------------------------------------------------------------------------------------^
 
-# Gets the centroid of the rotated line
-LineCentroid = getRoiMeasurements(RotatedLineRoi, Projected, CentroidString)
+	# Need to use an overlay so it will rotate with the image
+	LineOverlay = Overlay(FeducialLine)
+	Imp.setOverlay(LineOverlay)
 
-# Gets the width of the image
-Width = Imp.getWidth()
-# Creates a box roi that is 1 pixel high and the width of the image centred on the line centroid
-BoxRoi = Roi(0, LineCentroid[1], Width, 1)
+	# Rotates the image so the ladder is horizontal
+	IJ.run(Imp, "Arbitrarily...", "angle=" + str(FeducialAngle) + " interpolate stack")
+	# Gets the Rotated Roi from the overlay
+	RotatedLineOverlay = Imp.getOverlay()
+	RotatedLineRoi = RotatedLineOverlay.get(0)
+	# Removes the overlay to clean up the image
+	Imp.setOverlay(None)
 
-# Crops the image to the single line
-Imp.setRoi(BoxRoi)
-LineImage = Imp.crop("stack")
+	# Gets the centroid of the rotated line
+	LineCentroid = getRoiMeasurements(RotatedLineRoi, Projected, CentroidString)
 
-# Closes the original image to save memory
-Imp.close()
+	# Gets the width of the image
+	Width = Imp.getWidth()
+	# Creates a box roi that is 1 pixel high and the width of the image centred on the line centroid
+	BoxRoi = Roi(0, LineCentroid[1], Width, 1)
 
-# Runs the reslice command to get the XZ image similar to orthagonal view
-IJ.run(LineImage, "Reslice [/]...", "output=" + str(ZDepth) +" start=Top avoid")
+	# Crops the image to the single line
+	Imp.setRoi(BoxRoi)
+	LineImage = Imp.crop("stack")
 
-# Gets the resliced image
-selectWindow("Reslice ")
-OriginalSlicedImp = IJ.getImage()
-# Duplicates the image to only get one slice
-SlicedImp = OriginalSlicedImp.crop()
-# Close the original image to save memory
-OriginalSlicedImp.close()
+	# Closes the original image to save memory
+	Imp.close()
 
-# Performs gaussian blur to smooth the image
-IJ.run(SlicedImp, "Gaussian Blur...", "sigma=6")
-# Gets the statistics which includes the minimum and maximum intensity of the image
-ImpStats = SlicedImp.getStatistics()
-# Sets the prominence for the find maxima command to be half the difference between the min and max intensity
-Prominence = str((ImpStats.max - ImpStats.min)/2)
-# Finds the maxima in the image and outputs to a results table
-IJ.run(SlicedImp, "Find Maxima...", "prominence=" + Prominence + " output=List")
+	# Runs the reslice command to get the XZ image similar to orthagonal view
+	IJ.run(LineImage, "Reslice [/]...", "output=" + str(ZDepth) +" start=Top avoid")
 
-# Resets the contrast for easier viewing
-SlicedImp.resetDisplayRange()
-# Saves the XZ image and closes to save memory
-FileSaver(SlicedImp).saveAsTiff(os.path.join(OutputPath, FileNameNoExtension + "_XZ.tif"))
-SlicedImp.close()
+	# Gets the resliced image
+	selectWindow("Reslice ")
+	OriginalSlicedImp = IJ.getImage()
+	# Duplicates the image to only get one slice
+	SlicedImp = OriginalSlicedImp.crop()
+	# Close the original image to save memory
+	OriginalSlicedImp.close()
 
-# Gets the results table and copies it so the displayed one can be closed
-Results = ResultsTable().getResultsTable()
-MaximaResults = Results.clone()
-# Needs to reset the table to avoid dialog asking to save
-Results.reset()
-# Closes the results table
-WindowManager.getWindow("Results").close()
+	# Performs gaussian blur to smooth the image
+	IJ.run(SlicedImp, "Gaussian Blur...", "sigma=6")
+	# Gets the statistics which includes the minimum and maximum intensity of the image
+	ImpStats = SlicedImp.getStatistics()
+	# Sets the prominence for the find maxima command to be half the difference between the min and max intensity
+	Prominence = str((ImpStats.max - ImpStats.min)/2)
+	# Finds the maxima in the image and outputs to a results table
+	IJ.run(SlicedImp, "Find Maxima...", "prominence=" + Prominence + " output=List")
 
-# Calculates the axial step size for each maxima
-for Row in range(0, MaximaResults.size()):
-	AxialStep = MaximaResults.getValue("Y", Row) * ZDepth
-	MaximaResults.setValue("AxialStep", Row, AxialStep)
+	# Resets the contrast for easier viewing
+	SlicedImp.resetDisplayRange()
+	# Saves the XZ image and closes to save memory
+	FileSaver(SlicedImp).saveAsTiff(os.path.join(OutputPath, FileNameNoExtension + "_XZ.tif"))
+	SlicedImp.close()
 
-# Sorts the results table by the X coordinate
-MaximaResults.sort("X")
+	# Gets the results table and copies it so the displayed one can be closed
+	Results = ResultsTable().getResultsTable()
+	MaximaResults = Results.clone()
+	# Needs to reset the table to avoid dialog asking to save
+	Results.reset()
+	# Closes the results table
+	WindowManager.getWindow("Results").close()
 
-# Calculates the axial difference between each maxima
-for SortedRow in range(1, MaximaResults.size()):
-	AxialDiff = abs(MaximaResults.getValue("AxialStep", SortedRow) - MaximaResults.getValue("AxialStep", SortedRow - 1))
-	MaximaResults.setValue("AxialDiff", SortedRow, AxialDiff)
+	# Calculates the axial step size for each maxima
+	for Row in range(0, MaximaResults.size()):
+		AxialStep = MaximaResults.getValue("Y", Row) * ZDepth
+		MaximaResults.setValue("AxialStep", Row, AxialStep)
 
-# Saves the results table
-MaximaResults.saveAs(os.path.join(OutputPath, FileNameNoExtension + "_XZ.csv"))
+	# Sorts the results table by the X coordinate
+	MaximaResults.sort("X")
 
-# Resets the measurements to the original settings
-AnalyzerClass.setMeasurements(OriginalMeasurements)
+	# Calculates the axial difference between each maxima
+	for SortedRow in range(1, MaximaResults.size()):
+		AxialDiff = abs(MaximaResults.getValue("AxialStep", SortedRow) - MaximaResults.getValue("AxialStep", SortedRow - 1))
+		MaximaResults.setValue("AxialDiff", SortedRow, AxialDiff)
+
+	# Saves the results table
+	MaximaResults.saveAs(os.path.join(OutputPath, FileNameNoExtension + "_XZ.csv"))
+
+	# Resets the measurements to the original settings
+	AnalyzerClass.setMeasurements(OriginalMeasurements)
+
+if __name__ == "__main__":
+	main(InputImage, OutputDirectory)
